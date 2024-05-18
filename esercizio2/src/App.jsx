a2e0ed99e832e5051e9803c307a7f350ce617a80
simone_barbato <simone.barbato@webturing.net>
2024-05-18 17:58:10 +0200

    esercizio 2

---
+++ esercizio2/src/App.jsx
@@ -0,0 +1,15 @@
+
+import ListaSpesa from './components/ListaSpesa'
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return <>
+  <Header/>
+  <ListaSpesa/>
+  </>
+}
+
+export default App
---
+++ esercizio2/src/components/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles.css';
+
+export default function Header() {
+    return (
+      <div className="container">
+        <div className="custom-bg text-center text-danger">
+          <h1>Lista della spesa</h1>
+        </div>
+      </div>
+    );
+  }
+  
