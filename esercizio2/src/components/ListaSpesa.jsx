5c40ce8a3165ec175ad0b5d02eda5dcbc4e015c3
simone_barbato <simone.barbato@webturing.net>
2024-05-18 18:00:44 +0200

    sistemata indentazione

--- esercizio2/src/components/ListaSpesa.jsx
+++ esercizio2/src/components/ListaSpesa.jsx
@@ -15,22 +15,20 @@ export default function ListaSpesa() {
     return  (
         <div className='container mt-4'>
           {/* Itera attraverso ogni categoria nella shoppingData */}
-        {spesa.map((categoryData, index) => (
-          //L'elemento key è un attributo speciale in React che viene utilizzato per identificare univocamente 
-          //gli elementi figlio durante l'iterazione o la ricreazione di elenchi dinamici
-        <div key={index} className="category mb-3">
-          <h2 className="category-name">{categoryData.category}</h2>
-          <ul className="list-group">
-             {/* Itera attraverso ogni prodotto all'interno della categoria */}
-            {categoryData.items.map((item, idx) => (
-              <li key={idx} className="list-group-item">{item}</li>
-            ))}
-          </ul>
+          {spesa.map((categoryData, index) => (
+            //L'elemento key è un attributo speciale in React che viene utilizzato per identificare univocamente 
+            //gli elementi figlio durante l'iterazione o la ricreazione di elenchi dinamici
+          <div key={index} className="category mb-3">
+            <h2 className="category-name">{categoryData.category}</h2>
+            <ul className="list-group">
+              {/* Itera attraverso ogni prodotto all'interno della categoria */}
+              {categoryData.items.map((item, idx) => (
+                <li key={idx} className="list-group-item">{item}</li>
+              ))}
+            </ul>
         </div>
       ))}
-
-
-        </div>
+      </div>
 
     )
 }
