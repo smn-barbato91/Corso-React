5c4c0ff9bae790d8040e790742e081ee71a01dd7
lucawscuccu <lucawebservice@gmail.com>
2024-06-04 16:14:26 +0200

    change sidebar with NavLink

--- better-archibus/src/components/SideBar.jsx
+++ better-archibus/src/components/SideBar.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-
+import { NavLink } from "react-router-dom";
 
 function SideBar() {
  
@@ -12,53 +11,34 @@ function SideBar() {
         </a>
         <hr/>
         <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
+          <li className="nav-item ps-5 pb-3">
+            <NavLink to="/" className={({ isActive }) => isActive ? "text-white text-decoration-underline align-content-end" : "text-white text-decoration-none align-content-end"} >
               <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
-              Orders
-            </a>
+              Home
+            </NavLink>
           </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#grid"></use></svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#people-circle"></use></svg>
-              Customers
-            </a>
+          <li className="nav-item ps-5 pb-3">
+            <NavLink to="/employees" className={({ isActive }) => isActive ? "text-white text-decoration-underline align-content-end" : "text-white text-decoration-none align-content-end"}>
+            <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
+              Utenti
+            </NavLink>
           </li>
         </ul>
         <hr/>
         <div className="dropdown">
-          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
-            <strong>mdo</strong>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+          <NavLink className="d-flex align-items-center text-white text-decoration-none" to="/">
+            <img src="https://i.pinimg.com/736x/d0/76/79/d07679f66e8933049aa1cb888c5bd2da.jpg" alt="" width="32" height="32" className="rounded-circle me-2"/>
+            <strong>Green Team</strong>
+          </NavLink>
+          {/* <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
             <li><a className="dropdown-item" href="#">New project...</a></li>
             <li><a className="dropdown-item" href="#">Settings</a></li>
             <li><a className="dropdown-item" href="#">Profile</a></li>
             <li><hr className="dropdown-divider"/></li>
             <li><a className="dropdown-item" href="#">Sign out</a></li>
-          </ul>
+          </ul> */}
         </div>
       </div>
-      <div className="b-example-divider"></div>
     </>
   )
 }
