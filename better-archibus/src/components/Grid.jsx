304058d95ee7117cea674ab7e68bdf5432d0b3c1
simone_barbato <simone.barbato@webturing.net>
2024-06-04 17:23:47 +0200

    tabella dinamica

--- better-archibus/src/components/Grid.jsx
+++ better-archibus/src/components/Grid.jsx
@@ -1,47 +1,37 @@
 /* eslint-disable no-empty */
 import { useState } from "react";
 export default function Grid() {
+  const [listaEm, setListaEm] = useState([]);
 
-    const [listaEm,setListaEm] = useState(null);
+  fetch("http://localhost:3000/employees")
+    .then((response) => response.json()) // Read the response as text
+    .then((data) => setListaEm(data));
 
-    
-    fetch("http://localhost:3000/employees")
-        .then(response =>  response.json()) // Read the response as text
-        .then(data => setListaEm(JSON.stringify(data)))
-
-
-    console.log(JSON.parse(listaEm));
-    return (
-        <>
-       <table className="table vh-10">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        </>
-    )
+  //const listaEmArr = JSON.parse(listaEm);
+  return (
+    <>
+      <div>
+        <table className="table vh-10">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name First</th>
+              <th scope="col">Name Last</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaEm.map((element, index) => (
+              <tr key={index}>
+                <td>{element.em_id}</td>
+                <td>{element.name_first}</td>
+                <td>{element.name_last}</td>
+                <td>{element.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
